6d2ee8751520f5f5e1204513eb304e76df2a957e
Adrian <apeng2@hawaii.edu>
2022-04-09 12:39:40 -1000

    Finished header file for Cat class

--- Cat.h
+++ Cat.h
@@ -37,11 +37,28 @@ private: // Class Destructor
     void initializeMember();    // Zero out all member data
 
 public: // Public Methods
-    void print();                    // Print a cat
-    void validate();                 // Check if all of Cat member variables are valid
+    void print();               // Print a cat
+    void validate();            // Check if all of Cat member variables are valid
 
+public: // Validation functions
+    static int validateName(const char* newName);           // Function for checking if newName is valid
+    static int validateGender(const Gender newGender);      // Function for checking if newGender is valid
+    static int validateBreed(const Breed newBreed);         // Function for checking if newBreed is valid
+    static int validateWeight(const Weight newWeight);      // Function for checking if newWeight is valid
 
+public: // Getters (reportCat)
+    const char* getName();   // Get a Cat's name
+    Gender getGender();      // Get a Cat's gender
+    Breed getBreed();        // Get a Cat's breed
+    bool getFixed();         // Get if a cat is fixed
+    Weight getWeight();      // Get a Cat's weight
 
+public: // Setters (addCat)
+    void setName(const char* newName);  // Set a Cat's name
+    void setGender(Gender newGender);   // Set a Cat's gender       // Change this to protected later
+    void setBreed(Breed newBreed);      // Set a Cat's breed        // Change this to protected later
+    void setFixed();                    // Set if a cat is fixed
+    void setWeight(Weight newWeight);   // Set a Cat's weight
 
 };
 
