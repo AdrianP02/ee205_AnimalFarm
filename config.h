9ff5d329d45720f72ab65700328d5373f71e03a8
Adrian <apeng2@hawaii.edu>
2022-04-09 16:20:09 -1000

    Created a config file (Contents are what used to be in catDatabase.h). enum declarations, typedefs, and constants

---
+++ config.h
@@ -0,0 +1,34 @@
+///////////////////////////////////////////////////////////////////////////////
+///         University of Hawaii, College of Engineering
+/// @brief  ee205_lab_08d_animal_farm_1_to_clion - EE 205 - Spr 2022
+///
+/// @file config.h
+/// @version 1.0
+///
+/// @author Adrian Peng <apeng2@hawaii.edu>
+/// @date   09_Apr_2022
+///////////////////////////////////////////////////////////////////////////////
+#pragma once
+
+#define MAX_CATS 1024
+#define MAX_CAT_NAME_CHARACTERS 50
+
+typedef float Weight;
+typedef int NumCats;
+
+const Weight UNKNOWN_WEIGHT = -1; // When weight is unknown, set to -1
+
+enum Gender {
+    UNKNOWN_GENDER,
+    MALE,
+    FEMALE
+};
+
+enum Breed {
+    UNKNOWN_BREED,
+    MAINE_COON,
+    MANX,
+    SHORTHAIR,
+    PERSIAN,
+    SPHYNX
+};
