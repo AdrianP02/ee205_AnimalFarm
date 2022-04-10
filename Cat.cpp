e42a9719b61fb564ae98526d1817b1a94f0c19ba
Adrian <apeng2@hawaii.edu>
2022-04-09 14:47:11 -1000

    Finished Cat class

--- Cat.cpp
+++ Cat.cpp
@@ -10,3 +10,173 @@
 ///////////////////////////////////////////////////////////////////////////////
 #include "Cat.h"
 #include "catDatabase.h"
+
+#include <iostream>
+#include <cstring>
+#include <iomanip>
+
+using namespace std;
+
+// Create a cat with default values
+Cat::Cat() {
+    memset(name, 0, MAX_CAT_NAME_CHARACTERS);       // Set the name to be empty
+    gender = UNKNOWN_GENDER;
+    breed = UNKNOWN_BREED;
+    isCatFixed = false;
+    weight = UNKNOWN_WEIGHT;    // UNKNOWN_WEIGHT = -1
+    next = nullptr;             // Since this is now a default slot, the pointer to this slot will be nullptr
+}
+
+// Create a cat with chosen parameters
+Cat::Cat(const char* newName, const Gender newGender, const Breed newBreed, const Weight newWeight) : Cat() {
+    setName(newName);
+    setGender(newGender);
+    setBreed(newBreed);
+    setWeight(newWeight);
+}
+// Zero out/initialize cats
+void Cat::initializeMember() {          // Same contents as Cat::Cat()
+    memset(name, 0, MAX_CAT_NAME_CHARACTERS);
+    gender = UNKNOWN_GENDER;
+    breed = UNKNOWN_BREED;
+    isCatFixed = false;
+    weight = UNKNOWN_WEIGHT;
+    next = nullptr;
+}
+
+// PrintCat Formatting setup
+#define FORMAT_LINE( className, member ) cout << setw(8) << (className) << setw(20) << (member) << setw(52)
+bool Cat::print() {
+    validate();
+
+    cout << setw(80) << setfill( '=' ) << "" << endl ;
+    cout << setfill( ' ' ) ;
+    cout << left ;
+    cout << boolalpha ;
+    FORMAT_LINE( "Cat", "name" )         << getName()   << endl ;
+    FORMAT_LINE( "Cat", "gender" )       << genderName( getGender() ) << endl ;
+    FORMAT_LINE( "Cat", "breed" )        << breedName( getBreed() )   << endl ;
+    FORMAT_LINE( "Cat", "isFixed" )      << isFixed()   << endl ;
+    FORMAT_LINE( "Cat", "weight" )       << getWeight() << endl ;
+
+    return true ;
+}
+
+// Calls all validation functions
+void Cat::validate() {
+    validateName(name);
+    validateGender(gender);
+    validateBreed(breed);
+    validateWeight(weight);
+}
+
+// Validate Name
+// Test newName to see if it is valid
+// If it passes all tests, return true
+// *Just transfer over the functions from addCat and edit params
+bool Cat::validateName(const char *newName) {
+    // Is newName == NULL
+    if (newName == nullptr) {
+        cout << "AnimalFarm2: Name cannot be NULL" << endl;
+        return false;
+    }
+    // Is newName long (Longer than MAX_CAT_NAME_CHARACTERS)
+    if (strlen(newName) > MAX_CAT_NAME_CHARACTERS) {
+        cout << "AnimalFarm2: Name is too long. Maximum characters allowed is: " << MAX_CAT_NAME_CHARACTERS << endl;
+        return false;
+    }
+    // Is newName too short or 0
+    if (strlen(newName) <= 0) {
+        cout << "AnimalFarm2: Name cannot be blank "<< endl;
+        return false;
+    }
+    return true;
+}
+
+// Validate Gender
+// Gender must be known
+bool Cat::validateGender(const Gender newGender) {
+    if (newGender == UNKNOWN_GENDER) {
+        cout << "AnimalFarm2: Gender must be known" << endl;
+        return false;
+    }
+    return true;
+}
+
+// Validate Breed
+// Breed must be known
+bool Cat::validateBreed(const Breed newBreed) {
+    if (newBreed == UNKNOWN_BREED) {
+        cout << "AnimalFarm2: Breed must be known" << endl;
+        return false;
+    }
+    return true;
+}
+
+// Validate Weight
+// Weight must be > 0
+bool Cat::validateWeight(const Weight newWeight) {
+    if (newWeight <= 0) {
+        cout << "AnimalFarm2: Weight must be >= 0" << endl;
+        return false;
+    }
+    return true;
+}
+
+// Getters
+const char* Cat::getName() {
+    return name;
+}
+
+Gender Cat::getGender() {
+    return gender;
+}
+
+Breed Cat::getBreed() {
+    return breed;
+}
+
+bool Cat::getFixed() {
+    return isCatFixed;
+}
+
+Weight Cat::getWeight() {
+    return weight;
+}
+
+
+// Setters
+void Cat::setName(const char *newName) {
+    // Validate newName first
+    validateName(newName);
+    strcpy(name, newName);
+}
+
+void Cat::setGender(Gender newGender) {
+    if (gender == UNKNOWN_GENDER) {
+        validateGender(newGender);
+        Cat::gender = newGender;
+    }
+    else {
+        cout << "AnimalFarm2: Gender is already known and cannot be changed" << endl;
+    }
+}
+
+void Cat::setBreed(Breed newBreed) {
+    if (breed == UNKNOWN_BREED) {
+        validateBreed(newBreed);
+        Cat::breed = newBreed;
+    }
+    else {
+        cout << "AnimalFarm2: Breed is already known and cannot be changed" << endl;
+    }
+}
+
+void Cat::setFixed() {
+    Cat::isCatFixed == true;
+}
+
+void Cat::setWeight(Weight newWeight) {
+    validateWeight(newWeight);
+    Cat::weight = newWeight;
+}
--- Cat.h
+++ Cat.h
@@ -28,36 +28,36 @@ public: // Class Constructors
     Cat();                      // Construct a cat with default values
 
     Cat(const char* newName,    // Construct a cat with the following parameters
-        const Gender newGender, // Note: This should first set the fields to default values before changing them to
-        const Breed newBreed,   //       the parameters
-        const Weight newWeight  //
-        );
+        Gender newGender,       // Note: We do not have 'Fixed' in here because we can only fix a cat if it's not. We cannot unfix a cat... yet...
+        Breed newBreed,         //       We also don't have any info on if the Cats are fixed or not on the spec sheet
+        Weight newWeight
+    );
 
 private: // Class Destructor
     void initializeMember();    // Zero out all member data
 
 public: // Public Methods
-    void print();               // Print a cat
-    void validate();            // Check if all of Cat member variables are valid
+    bool print();               // Print a cat
+    void validate();            // Check if all of Cat member variables are valid (Runs all validation functions)
 
-public: // Validation functions
-    static int validateName(const char* newName);           // Function for checking if newName is valid
-    static int validateGender(const Gender newGender);      // Function for checking if newGender is valid
-    static int validateBreed(const Breed newBreed);         // Function for checking if newBreed is valid
-    static int validateWeight(const Weight newWeight);      // Function for checking if newWeight is valid
+public: // Validation functions (Port from AnimalFarm1 files)
+    static bool validateName(const char* newName);          // Function for checking if newName is valid
+    static bool validateGender(const Gender newGender);      // Function for checking if newGender is valid
+    static bool validateBreed(const Breed newBreed);         // Function for checking if newBreed is valid
+    static bool validateWeight(const Weight newWeight);      // Function for checking if newWeight is valid
 
-public: // Getters (reportCat)
+public: // Getters
     const char* getName();   // Get a Cat's name
     Gender getGender();      // Get a Cat's gender
     Breed getBreed();        // Get a Cat's breed
-    bool getFixed();         // Get if a cat is fixed
+    bool getFixed();         // See if a Cat is fixed
     Weight getWeight();      // Get a Cat's weight
 
-public: // Setters (addCat)
+public: // Setters
     void setName(const char* newName);  // Set a Cat's name
     void setGender(Gender newGender);   // Set a Cat's gender       // Change this to protected later
     void setBreed(Breed newBreed);      // Set a Cat's breed        // Change this to protected later
-    void setFixed();                    // Set if a cat is fixed
+    void setFixed();                    // Fix a Cat
     void setWeight(Weight newWeight);   // Set a Cat's weight
 
 };
