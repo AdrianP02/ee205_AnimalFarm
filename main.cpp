8e78217708f7ab76ac54a64be4d146885c6ae22c
Adrian <apeng2@hawaii.edu>
2022-04-10 14:02:47 -1000

    Finished up basic main() program

--- main.cpp
+++ main.cpp
@@ -4,30 +4,20 @@
 #include "reportCat.h"
 #include "updateCat.h"
 #include "deleteCat.h"
+#include "Cat.h"
 
 #include <stdio.h>
 
 int main() {
     printf("Starting Animal Farm 2\n");
 
-    addCat( "Loki", MALE, PERSIAN, true, 8.5, BLACK, WHITE, 101 ) ;
-    addCat( "Milo", MALE, MANX, true, 7, BLACK, RED, 102 ) ;
-    addCat( "Bella", FEMALE, MAINE_COON, true, 18.2, BLACK, BLUE, 103 ) ;
-    addCat( "Kali", FEMALE, SHORTHAIR, false, 9.2, BLACK, GREEN, 104 ) ;
-    addCat( "Trin", FEMALE, MANX, true, 12.2, BLACK, PINK, 105 ) ;
-    addCat( "Chili", UNKNOWN_GENDER, SHORTHAIR, false, 19.0, WHITE, BLACK, 106 ) ;
+    addCat( new Cat( "Loki", MALE, PERSIAN, 1.0 )) ;
+    addCat( new Cat( "Milo", MALE, MANX , 1.1 )) ;
+    addCat( new Cat( "Bella", FEMALE, MAINE_COON, 1.2 )) ;
+    addCat( new Cat( "Kali", FEMALE, SHORTHAIR, 1.3 )) ;
+    addCat( new Cat( "Trin", FEMALE, MANX, 1.4 )) ;
+    addCat( new Cat( "Chili", MALE, SHORTHAIR, 1.5 )) ;
 
-    int kali = findCat("Kali");
-    printAllCats() ;
-    findCat("Kali");
-    updateCatName( kali, "Chili" ) ;
-    printCat( kali );
-    updateCatName( kali, "Capulet" ) ;
-    updateCatWeight( kali, 9.9 ) ;
-    fixCat( kali ) ;
-    updateCatCollar1( kali, GREEN);
-    updateCatCollar2(kali, GREEN);
-    printCat( kali );
     printAllCats();
     deleteAllCats();
     printAllCats();
