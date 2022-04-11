8e680af8ebf7adb1eca863697474ba6854d69ead
Adrian <apeng2@hawaii.edu>
2022-04-10 14:04:48 -1000

    Moved entire updateCat functionality into Cat.cpp 'Setters'

--- updateCat.h
+++ updateCat.h
@@ -8,16 +8,16 @@
 /// @author Adrian Peng <apeng2@hawaii.edu>
 /// @date   11_Mar_2022
 ///////////////////////////////////////////////////////////////////////////////
-#pragma once
-
-#include "catDatabase.h"
-
-#include <stdbool.h>
-
-extern bool updateCatName(int index, const char* name);
-extern bool fixCat(int index);
-extern bool updateCatWeight(int index, Weight weight);
-extern bool updateCatCollar1(int index, const enum Color color);
-extern bool updateCatCollar2(int index, const enum Color color);
-extern bool updateLicense(int index, const unsigned long long license);
-extern bool isCatNameTaken2(const char* name);
+//#pragma once
+//
+//#include "catDatabase.h"
+//
+//#include <stdbool.h>
+//
+//extern bool updateCatName(int index, const char* name);
+//extern bool fixCat(int index);
+//extern bool updateCatWeight(int index, Weight weight);
+//extern bool updateCatCollar1(int index, const enum Color color);
+//extern bool updateCatCollar2(int index, const enum Color color);
+//extern bool updateLicense(int index, const unsigned long long license);
+//extern bool isCatNameTaken2(const char* name);
