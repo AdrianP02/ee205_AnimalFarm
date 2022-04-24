550b247a47b5c5d4b372faca9a3f2f1d6213d1b6
Adrian <apeng2@hawaii.edu>
2022-04-23 15:48:38 -1000

    Completed header file with all requested functions

--- Node.h
+++ Node.h
@@ -8,13 +8,40 @@
 /// @author Adrian Peng <apeng2@hawaii.edu>
 /// @date   23_Apr_2022
 ///////////////////////////////////////////////////////////////////////////////
-#ifndef EE205_ANIMALFARM_NODE_H
-#define EE205_ANIMALFARM_NODE_H
+#pragma once
 
+#include "config.h"
+
+#include <iostream>
 
 class Node {
+/// Friends
+friend class List;
+friend class SinglyLinkedList;
+
+/// Public Member Functions
+public:
+    // Output the contents of this object
+    virtual void dump() const;
+
+    // Check and validate the Node
+    virtual bool validate() const noexcept;
+
+    // Compare two nodes. Is left side > than right side
+    virtual bool operator>(const Node &rightSide);
+
+
+/// Static Protected Member Functions
+protected:
+    // Compare memory address of the object
+    static bool compareByAddress(const Node *node1, const Node *node2);
+
+
+/// Protected Attributes
+protected:
+    Node* next = nullptr;
 
 };
 
 
-#endif //EE205_ANIMALFARM_NODE_H
+
