54728437f7c890f80efdbb2664f48e1eb56453ce
Adrian <apeng2@hawaii.edu>
2022-04-24 13:57:23 -1000

    Completed List. hopefully

--- List.cpp
+++ List.cpp
@@ -9,3 +9,72 @@
 /// @date   23_Apr_2022
 ///////////////////////////////////////////////////////////////////////////////
 #include "List.h"
+
+using namespace std;
+
+/// Public member functions
+// Checks if the list is empty
+bool List::empty() const noexcept {
+    if (head == nullptr) {
+        return true;
+    }
+    else {
+        return false;
+    }
+}
+
+
+// Get the number of Node objects in the List
+unsigned int List::size() const noexcept {
+    return count;
+}
+
+
+// Checks if aNode is in the list
+bool List::isIn(Node *aNode) const {
+    Node* currentNode = head;
+    while (currentNode != nullptr) {
+        if (currentNode == aNode) {
+            return true;
+        }
+        currentNode = currentNode -> next;
+    }
+    return false;
+}
+
+
+// Checks if the list is sorted
+bool List::isSorted() const noexcept {
+    if (count == 1) {
+        return true;
+    }
+    for(Node* i = head; i -> next != nullptr; i = i -> next) {
+        if(*i > *i -> next) {
+            return false;
+        }
+    }
+    return true;
+}
+
+
+// Get the first Node in the List
+Node* List::get_first() const noexcept {
+    return head;
+}
+
+
+// Delete all nodes in the List
+void List::deleteAllNodes() noexcept {
+    while(head != nullptr) {
+        pop_front();
+    }
+}
+
+
+
+/// Static Public Member Functions
+// Get the next node in the list
+Node* List::get_next(const Node *currentNode) {
+    return currentNode -> next;
+}
+
--- List.h
+++ List.h
@@ -8,13 +8,56 @@
 /// @author Adrian Peng <apeng2@hawaii.edu>
 /// @date   23_Apr_2022
 ///////////////////////////////////////////////////////////////////////////////
-#ifndef EE205_ANIMALFARM_LIST_H
-#define EE205_ANIMALFARM_LIST_H
+#pragma once
 
+#include "config.h"
+#include "Node.h" // The friend class
 
 class List {
+    /// Public Member Functions
+public:
+    // Checks if the list is empty
+    bool empty() const noexcept;
 
+    // Get the number of Node objects in the List
+    unsigned int size() const noexcept;
+
+    // Checks if a node is in the list
+    bool isIn(Node *aNode) const;
+
+    // Checks if the list is sorted
+    bool isSorted() const noexcept;
+
+    // Get the first Node in the List
+    Node* get_first() const noexcept;
+
+    // Delete all nodes in the List
+    void deleteAllNodes() noexcept;
+
+    // Remove and return the first Node in the List
+    virtual Node* pop_front() noexcept = 0;
+
+    // Output the contents of this container
+    virtual void dump() const noexcept = 0;
+
+    // Validation
+    virtual bool validate() const noexcept = 0;
+
+
+
+    /// Static Public Member Functions
+public:
+    // Get the next node in the list
+    static Node* get_next(const Node *currentNode);
+
+
+
+    /// Protected Attributes
+protected:
+    // The head pointer for the collection
+    Node* head = nullptr;
+
+    // Maintain a count of the number of Node objects in the collection
+    unsigned int count = 0;
 };
 
-
-#endif //EE205_ANIMALFARM_LIST_H
