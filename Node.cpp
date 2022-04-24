2c8570b73707890bd783bc65c18f4c726b537171
Adrian <apeng2@hawaii.edu>
2022-04-23 17:07:44 -1000

    Completed Node class

--- Node.cpp
+++ Node.cpp
@@ -9,3 +9,36 @@
 /// @date   23_Apr_2022
 ///////////////////////////////////////////////////////////////////////////////
 #include "Node.h"
+#include "config.h"
+
+using namespace std;
+
+/// Public Member Functions
+// Output the contents of this object
+void Node::dump() const {
+    FORMAT_LINE_FOR_DUMP("Node", "this") << this << endl;
+    FORMAT_LINE_FOR_DUMP("Node", "next") << next << endl;
+}
+
+// Check and validate the Node
+bool Node::validate() const noexcept {
+
+}
+
+// Compare two nodes. Is left side > than right side
+bool Node::operator>(const Node &rightSide) {
+    return compareByAddress( this, &(Node&)rightSide );
+}
+
+
+/// Static Protected Member Functions
+// Compare memory address of the object
+bool Node::compareByAddress(const Node *node1, const Node *node2) {
+    if (node1 > node2) {
+        return true;
+    }
+    else {
+        return false;
+    }
+}
+
--- Node.h
+++ Node.h
@@ -42,6 +42,3 @@ protected:
     Node* next = nullptr;
 
 };
-
-
-
