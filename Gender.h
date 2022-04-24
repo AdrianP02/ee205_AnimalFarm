4be09d56d6b79509ec60f581f831da655e803e47
Adrian <apeng2@hawaii.edu>
2022-04-23 14:26:26 -1000

    Created Gender Class

---
+++ Gender.h
@@ -0,0 +1,20 @@
+///////////////////////////////////////////////////////////////////////////////
+///         University of Hawaii, College of Engineering
+/// @brief  ee205_lab_08d_animal_farm_1_to_clion - EE 205 - Spr 2022
+///
+/// @file Gender.h
+/// @version 1.0
+///
+/// @author Adrian Peng <apeng2@hawaii.edu>
+/// @date   23_Apr_2022
+///////////////////////////////////////////////////////////////////////////////
+#ifndef EE205_LAB_08D_ANIMAL_FARM_1_TO_CLION_GENDER_H
+#define EE205_LAB_08D_ANIMAL_FARM_1_TO_CLION_GENDER_H
+
+
+class Gender {
+
+};
+
+
+#endif //EE205_LAB_08D_ANIMAL_FARM_1_TO_CLION_GENDER_H
