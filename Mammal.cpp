fcb55a5a2cc153c0d1a4752680a42284f9d59120
Adrian <apeng2@hawaii.edu>
2022-04-24 20:54:20 -1000

    Completed Mammal Class

--- Mammal.cpp
+++ Mammal.cpp
@@ -9,3 +9,38 @@
 /// @date   23_Apr_2022
 ///////////////////////////////////////////////////////////////////////////////
 #include "Mammal.h"
+#include "config.h"
+
+#include <string>
+
+using namespace std;
+
+/// Public Member Functions
+// Create a Mammal with the minimum required fields
+
+// Create a Mammal, populating all of the member variables
+
+// Get the color
+Color Mammal::getColor() const noexcept {
+    return color;
+}
+
+// Set the color
+void Mammal::setColor(const Color newColor) noexcept {
+    color = newColor;
+}
+
+// Print the contents of this object and parent
+void Mammal::dump() const noexcept {
+    Animal::dump();
+    FORMAT_LINE_FOR_DUMP("Mammal", "color") << color << endl;
+}
+
+
+/// Static Public Attributes
+// The scientific name for mammals
+const string Mammal::MAMMAL_NAME = "Mammilian";
+
+
+/// Protected Attributes
+// The primary color of the mammal
--- Mammal.h
+++ Mammal.h
@@ -8,13 +8,48 @@
 /// @author Adrian Peng <apeng2@hawaii.edu>
 /// @date   23_Apr_2022
 ///////////////////////////////////////////////////////////////////////////////
-#ifndef EE205_ANIMALFARM_MAMMAL_H
-#define EE205_ANIMALFARM_MAMMAL_H
+#pragma once
+
+#include "config.h"
+#include "Color.h"
+#include "Animal.h"
+
+#include <string>
+
+using namespace std;
+
+class Mammal : public Animal {
+/// Public Member Functions
+public:
+    // Create a Mammal with the minimum required fields
+    Mammal(const Weight::t_weight newMaxWeight, const string& newSpecies) : Animal(newMaxWeight, MAMMAL_NAME, newSpecies) {};
+
+    // Create a Mammal, populating all of the member variables
+    Mammal (const Color newColor, const Gender newGender, const Weight::t_weight newWeight, const Weight::t_weight newMaxWeight, const string& newSpecies) : Animal(newGender, newWeight, newMaxWeight, MAMMAL_NAME, newSpecies){
+        setColor(newColor);
+    };
+
+    // Get the color
+    Color getColor() const noexcept;
+
+    // Set the color
+    void setColor(const Color newColor) noexcept;
+
+    // Print the contents of this object and parent
+    void dump() const noexcept override;
 
 
-class Mammal {
+/// Static Public Attributes
+public:
+    // The scientific name for mammals
+    static const string MAMMAL_NAME;
+
+/// Protected Attributes
+protected:
+
+    // The primary color of the mammal
+    Color color = Color::UNKNOWN_COLOR;
 
 };
 
 
-#endif //EE205_ANIMALFARM_MAMMAL_H
